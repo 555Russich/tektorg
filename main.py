f5d4a42d155f42cf092ec40df2347c6a5ee81cc4
russich555 <Kutsokondima@yandex.ru>
2022-12-12 10:58:32 +0300

    check status code

--- main.py
+++ main.py
@@ -66,6 +66,8 @@ async def get_procedures_urls(s: aiohttp.ClientSession) -> list:
     while not appended_all_new:
         procedures_urls_to_append_temp = procedures_urls_to_append.copy()
         async with s.get('https://www.tektorg.ru/rosneft/procedures', params=params) as r:
+            if r.status != 200:
+                raise ConnectionError(f'{r.status=}')
             soup = BeautifulSoup(await r.text(), 'lxml')
 
         if not last_page_number:
@@ -92,67 +94,68 @@ async def get_procedures_urls(s: aiohttp.ClientSession) -> list:
 
 
 async def handle_procedure(s: aiohttp.ClientSession, url: str) -> bool:
+    logging.info(f'Start collecting data for {url}')
     async with s.get(url) as r:
-        logging.info(f'Start collecting data for {url}')
-
+        if r.status != 200:
+            raise ConnectionError(f'{r.status=}')
         soup = BeautifulSoup(await r.text(), 'lxml')
-        if 'Вы не авторизированы для доступа к этой странице' in soup.text:
-            logging.info('Unauthorized')
-            return False
 
-        procedure_number = soup.find('td', text='Номер закупки:').find_next('td').text
-        procedure_name = soup.find('span', class_='procedure__item-name').text
+    if 'Вы не авторизированы для доступа к этой странице' in soup.text:
+        logging.info('Unauthorized')
+        return False
 
-        Path(DIR_PROCEDURES, procedure_number).mkdir(exist_ok=True)
-        append_row_to_xlsx(
-            Path(DIR_PROCEDURES, FILENAME_XLSX),
-            {
-                'Номер процедуры': procedure_number,
-                'Наименование закупки': procedure_name
-            }
-        )
+    procedure_number = soup.find('td', text='Номер закупки:').find_next('td').text
+    procedure_name = soup.find('span', class_='procedure__item-name').text
 
-        files_data = []
-        filenames = []
-        for doc in soup.find_all('div', class_='procedure__item--documents-item'):
-            a_doc = doc.find('div', class_='item-name').find('a')
-            doc_url = 'https://tektorg.ru' + a_doc.get('href')
+    Path(DIR_PROCEDURES, procedure_number).mkdir(exist_ok=True)
+    append_row_to_xlsx(
+        Path(DIR_PROCEDURES, FILENAME_XLSX),
+        {
+            'Номер процедуры': procedure_number,
+            'Наименование закупки': procedure_name
+        }
+    )
 
-            if 'docprotocol' in doc_url:
-                extension = re.search(r'(?<=\()[a-zA-Z\d]+(?=\)$)', a_doc.text).group(0)
-                filename = a_doc.text.replace(f'({extension})', '')
-                filename = ''.join(list(filename)[:-1]) if list(filename)[-1] == ' ' else filename
-                filename = f"{filename}.{extension}"
-            else:
-                filename = re.search(r'(?<=\().+(?=\))', a_doc.text).group(0) \
-                    if re.search(r'(?<=\().+(?=\))', a_doc.text) else a_doc.text
-            filename = re.sub(r'[<>:"/\\|?*]', '', filename)
-            filepath = Path(DIR_PROCEDURES, procedure_number, filename)
+    files_data = []
+    filenames = []
+    for doc in soup.find_all('div', class_='procedure__item--documents-item'):
+        a_doc = doc.find('div', class_='item-name').find('a')
+        doc_url = 'https://tektorg.ru' + a_doc.get('href')
 
-            same_filenames = [x for x in filenames if filename in x]
-            if len(same_filenames) > 0:
-                filepath = Path(
-                    filepath.parent, f'{filepath.stem}_{len(same_filenames)}'
-                ).with_suffix(filepath.suffix)
-            filenames.append(filename)
-
-            files_data.append({'url': doc_url, 'path': filepath})
-
-        for retry in range(1, RETRIES+1):
-            tasks_download = [download_file(s, d['url'], d['path']) for d in files_data]
-            results = await asyncio.gather(*tasks_download, return_exceptions=True)
-
-            for i in reversed(range(len(results))):
-                if results[i] is True:
-                    del files_data[i]
-
-            if not files_data:
-                break
-            time.sleep(10)
-            logging.warning(f'Retry №:{retry} while downloading files. Tasks count: {len(tasks_download)}')
+        if 'docprotocol' in doc_url:
+            extension = re.search(r'(?<=\()[a-zA-Z\d]+(?=\)$)', a_doc.text).group(0)
+            filename = a_doc.text.replace(f'({extension})', '')
+            filename = ''.join(list(filename)[:-1]) if list(filename)[-1] == ' ' else filename
+            filename = f"{filename}.{extension}"
         else:
-            logging.error('ERROR while downloading files')
-            raise ConnectionError('ERROR while downloading files')
+            filename = re.search(r'(?<=\().+(?=\))', a_doc.text).group(0) \
+                if re.search(r'(?<=\().+(?=\))', a_doc.text) else a_doc.text
+        filename = re.sub(r'[<>:"/\\|?*]', '', filename)
+        filepath = Path(DIR_PROCEDURES, procedure_number, filename)
+
+        same_filenames = [x for x in filenames if filename in x]
+        if len(same_filenames) > 0:
+            filepath = Path(
+                filepath.parent, f'{filepath.stem}_{len(same_filenames)}'
+            ).with_suffix(filepath.suffix)
+        filenames.append(filename)
+
+        files_data.append({'url': doc_url, 'path': filepath})
+
+    for retry in range(1, RETRIES+1):
+        tasks_download = [download_file(s, d['url'], d['path']) for d in files_data]
+        results = await asyncio.gather(*tasks_download, return_exceptions=True)
+
+        for i in reversed(range(len(results))):
+            if results[i] is True:
+                del files_data[i]
+
+        if not files_data:
+            break
+        time.sleep(10)
+        logging.warning(f'Retry №:{retry} while downloading files. Tasks count: {len(tasks_download)}')
+    else:
+        raise ConnectionError('ERROR while downloading files')
     return True
 
 
@@ -180,7 +183,7 @@ def sync_collect_data():
 def looping_collect_data():
     sync_collect_data()
 
-    schedule.every().day.at('13:00').do(sync_collect_data)
+    schedule.every().day.at('20:00').do(sync_collect_data)
     while True:
         schedule.run_pending()
         time.sleep(1)
